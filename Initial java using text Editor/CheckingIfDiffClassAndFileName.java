328ccf5c9dbafd0d5b1b8ea3f2699f488a5cb7aa
SANVIOJHA <sanviojhasemaria@gmail.com>
2026-03-08 12:43:41 +0530

    First is with public class and CheckingIfDiffClassAndFileName is without publicc class
    git push origin main#

---
+++ Initial java using text Editor/CheckingIfDiffClassAndFileName.java
@@ -0,0 +1,6 @@
+import java.lang.*;
+class Second{
+public static void main(String[] args){
+System.out.println("in java we can have different name for class and file but the class should not be public ...but when we declared the class name as public then the class name and the file name should be same");
+}
+}
---
+++ Initial java using text Editor/First.java
@@ -0,0 +1,6 @@
+import java.lang.*;
+public class First{
+public static void main(String[] args){
+System.out.println("in java we can have different name for class and file but the class should not be public ...but when we declared the class name as public then the class name and the file name should be same");
+}
+}
